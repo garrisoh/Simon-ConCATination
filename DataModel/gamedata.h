3fe03307d0afb0dba8d2e4353b3162bcad3d8140
Benji Nitkin <bnitkin@gmail.com>
2014-04-22 17:19:41 -0400

    Moved documentation for DataModels

--- DataModel/gamedata.h
+++ DataModel/gamedata.h
@@ -18,22 +18,101 @@ private:
     std::vector<float> userTimes;
 
 public:
+    /**
+         @brief Constructor for a GameData object
+         @param color The colorType to use
+         @param sound The soundType to use
+         @param interface The interfaceType to use
+         @param record Whether or not to record this trial (false==demo mode)
+    */
     GameData(ColorType color, SoundType sound, InterfaceType interface, bool record);
+
+    /**
+         @brief Copy constructor for gameData
+         @param copy A reference to a gameData to clone.
+    */
     GameData(const GameData &copy);
+
+    /**
+     * @brief Returns the list of computer-generated quadrants, for lighting up the board
+     * @return Complete quadrant list
+     */
     std::vector<QuadrantID> getQuadrants();
+
+    /**
+     * @brief Sets the color mode
+     * @param color Color mode, from enum
+     */
     void setColor(ColorType color);
+
+    /**
+     * @brief Returns the color mode
+     * @return The current color mode
+     */
     ColorType getColor();
+
+    /**
+     * @brief Sets the sound mode
+     * @param sound Sound mode, from enum
+     */
     void setSound(SoundType sound);
+
+    /**
+     * @brief Returns the sound mode
+     * @return The current sound mode
+     */
     SoundType getSound();
+
+    /**
+      @brief Sets the interface type
+      Availiable interfaces are keyboard, mouse, and leap. See globals.h for a
+      listing of enums.
+      @param interface The interface to use.
+      */
     void setInterfaceType(InterfaceType interface);
+
+    /**
+      @brief Gets the current interface
+      @return The interface in use.
+      Lookup interfaces in globals.h
+      */
     InterfaceType getInterface();
+
+    /**
+     * @brief Sets whether to record this trial
+     * @param record Whether this trial will be committed to CSV
+     */
     void setRecord(bool record);
+
+    /**
+     * @brief Returns the current record mode
+     * @return Whether this trial will be committed to CSV
+     */
     bool getRecord();
 
+    /**
+     * @brief Exports all collected data as a CSV string
+     * @return CSV string representing the GameData
+     */
     std::string toCSV();
 
+    /**
+     * @brief Adds a quadrant to the computer-generated list
+     * @param quadrant Quadrant ID
+     */
     void addQuadrant(QuadrantID quadrant);
+
+    /**
+     * @brief Adds a quadrant to the user-generated list. This is compared to the computer one and reported to Nees.
+     * @param quadrant Quadrant ID
+     */
     void addUserQuadrant(QuadrantID quadrant);
+
+    /**
+     * @brief Adds a time to the user-generated list, to be paired with quadrant presses.
+     * This is compared to the computer one and reported to Nees.
+     * @param time When the user clicked.
+     */
     void addUserTime(float time);
 };
 
--- DataModel/trialdata.cpp
+++ DataModel/trialdata.cpp
@@ -3,8 +3,7 @@
 //Populate singleton
 TrialData* TrialData::currentTrial = new TrialData();
 
-TrialData::TrialData()
-{
+TrialData::TrialData() {
     this->gender = false;
     this->age = 0;
     this->participantID = "";
@@ -15,108 +14,57 @@ TrialData::~TrialData() {
 }
 
 TrialData* TrialData::getCurrentTrial() {
-    /**
-     * @brief Grabs a copy of the currently active trialData.
-     * @return A reference to the active TrialData
-     */
     return TrialData::currentTrial;
 }
 
 void TrialData::newTrial() {
-    /**
-     * @brief Resets all gathered data: user metrics, times, quadrants, and so on.
-     */
     delete TrialData::currentTrial;
     TrialData::currentTrial = new TrialData();
 }
 
 GameData* TrialData::getGame(int index) {
-    /**
-     * @brief Fetch a game from the internal list.
-     * @return A reference to the game
-     * @param index The array index of the game to fetch.
-     */
     return &this->games[index];
 }
 
 int TrialData::getNumberGames() {
-	/** 
-	 * @brief The number of games in the internal game array.
-	 * @return Number of games in the array.
-	 */
 	return this->games.size();
 }
 
 void TrialData::setPID(std::string pid) {
-    /**
-     * @brief Sets the subject's participant ID in the record
-     * @param pid The subject's participant ID
-     */
     this->participantID = pid;
 }
 
 void TrialData::setGender(bool gender) {
-    /**
-     * @brief Sets the subject's gender in the record
-     * @param gender The subject's gender
-     */
     this->gender = gender;
 }
 
 void TrialData::setAge(int age) {
-    /**
-     * @brief Sets the subject's age in the record.
-     * @param age The subject's age
-     */
     this->age = age;
 }
 
 void TrialData::setOutFile(std::string outFile) {
-    /**
-     * @brief Sets the destanation location for the session report. No access testing is performed.
-     * @param outFile The path to the file to write, as a string.
-     */
     this->outFile = outFile;
 }
 
 void TrialData::addGame() {
-    /**
-     * @brief Appends a game to the list of games to be played.
-     */
     this->games.push_back(GameData(ColorTypeOn, SoundTypeRegular, InterfaceMouse, true));
 }
 
 void TrialData::addGame(GameData gd) {
-    /**
-     * @brief Appends a CUSTOM game to the list of games to be played.
-     */
     this->games.push_back(gd);
 }
 
 void TrialData::swapGames(int index1, int index2) {
-    /**
-     * @brief Swap Games in a list by index.
-     */
     GameData temp = this->games.at(index1);
     this->games.at(index1) = this->games.at(index2);
     this->games.at(index2) = temp;
-
 }
 
 void TrialData::removeGame(int index) {
-    /**
-     * @brief Removes a game from the running list
-     * @param index The index of the game to remove
-     */
     this->games.erase(games.begin()+index);
 }
 
 ErrorType TrialData::writeCSV() {
-    /**
-     * @brief Writes all current games out to memory.
-     * @return Whether the write was successful.
-     */
-
     //Check if the file exists on disk by trying to read it. 
     //If the read fails, open as a new file and write the header.
 
